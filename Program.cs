b8780c47064b132b53131d81b6e483dd554a93de
Dulce Alvarado <your email here>
2023-02-28 20:00:52 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace RunwithMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to RunwithMe!");
+            Console.WriteLine("\tDid you run today? Y/N");
+            var responseDidTheyRun = Console.ReadKey().KeyChar;
+            var run = new Run();
+
+            if (responseDidTheyRun == 'y')
+            {
+                run.Ran = true;
+                Console.WriteLine("Thanks for running!");
+            }
+            else if (responseDidTheyRun == 'n')
+            {
+                run.Ran = false;
+                Console.WriteLine("You should run more.");
+            }
+            else
+            {
+                Console.WriteLine("You entered an invalid option, logging error and quitting");
+                //Log error to txt file
+                return;
+            }
+
+            if (run.Ran == false) { return; }
+
+            Console.WriteLine("Since you ran, how far did you run? Enter distance in miles");
+            var responseHowFarRan = Console.ReadLine();
+            if (decimal.TryParse(responseHowFarRan, out decimal howFarRan))
+            {
+                Console.WriteLine("You entered an invalid option, logging error and quitting");
+                return;
+            }
+            run.Distance = howFarRan;
+
+            Console.WriteLine("How long did it take you to run?");
+            if (!TimeSpan.TryParse(Console.ReadLine(), out TimeSpan HowLongRan))
+            {
+                Console.WriteLine("You entered an invalid option, logging error and quitting");
+                return;
+            }
+            run.Duration = HowLongRan;
+
+            Console.WriteLine("Ran: = + run.Ran");
+            Console.WriteLine("Distance:" + run.Distance);
+            Console.WriteLine("Duration:" + run.Duration);
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+﻿# RunwithMe
+
+This project is an app that will record the distance, duration and dates of miles ran to track progress. The
+runner will need to carry a smart phone with them to track activity by inputting information to get reports. The features
+in this app will include:
+* A “master loop” where the user can repeatedly enter commands, including choosing to exit the program
+* A log that records errors, invalid inputs, or other important events and writes them to a text file
+* Calculated and displayed data based on an external factor 
